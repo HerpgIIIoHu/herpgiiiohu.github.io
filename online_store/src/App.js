47242a8d2f958c5325667a863d42e878d8c9a0c0
Konstantin <kolupaevkonstantin18@gmail.com>
2022-09-22 04:05:50 +0700

    Verstka_start

--- online_store/src/App.js
+++ online_store/src/App.js
@@ -1,7 +1,26 @@
 function App() {
   return (
-    <div className="App">
-      Hello!!!
+    <div className="Wrapper">
+      <header>
+        <div>
+          <svg/>
+          <div>
+            <h3>React Sneakers</h3>
+            <p>Store best sneakers</p>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <svg/>
+            <span>
+              1205 руб.
+            </span>
+          </li>
+          <li>
+            <svg/>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
