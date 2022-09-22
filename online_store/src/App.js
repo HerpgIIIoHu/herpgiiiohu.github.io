fc55a88b02038e5111900bab833bb0f4ce9768b1
Konstantin <kolupaevkonstantin18@gmail.com>
2022-09-23 03:53:37 +0700

    Home page succeess

--- online_store/src/App.js
+++ online_store/src/App.js
@@ -1,6 +1,148 @@
 function App() {
   return (
+
     <div className="wrapper clear">
+
+      <div style={{display: 'none'}} className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex justify-between">Корзина<a className="cu-p"><img width="32px" height="32px" src="img/close.svg" /></a></h2>
+          <div className="itemss">
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/2.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12999 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/4.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>8499 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/2.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12999 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/4.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>8499 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/2.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12999 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/4.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>8499 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/2.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12999 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+
+          <div className="cartItem d-flex align-center">
+            <div className="logoCart">
+              <img width="70px" height="70px" src="img/sneakers/4.jpg" />
+            </div>
+            
+            <div className="cart_text">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <b>8499 руб.</b>
+            </div>
+            <div className="closeBtn">
+              <a><img width="32px" height="32px" src="img/close.svg" /></a>
+            </div>
+          </div>
+
+          </div>
+          
+          <div className="price">
+            <ul>
+              <li className="d-flex justify-between">
+                <span>Итого:</span>
+                <b>21 498 руб.</b>
+              </li>
+              <li className="d-flex justify-between">
+                <span>Налог 5%:</span>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="green-btn" placeholder="Оформить заказ">Оформить заказ <img src="img/strelka.svg" /></button>
+          </div>
+        </div>
+      </div>
+
+
+
       <header className="d-flex justify-between align-center">
         <div className="headerLeft d-flex align-center">
           <img width="40px" height="40px" src="img/logo.png"/>
@@ -23,9 +165,19 @@ function App() {
         </ul>
       </header>
       <div className="content p-40 ">
-        <h1>Все кроссовки</h1>
+        <div className="d-flex align-center justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search d-flex">
+            <img src="img/search.svg"/>
+            <input placeholder="Поиск..."></input>
+          </div>
+        </div>
+        
         <div className="d-flex justify-between">
         <div className="card">
+          <div className="favorite">
+            <a href="#" className="aLikes"></a>
+          </div>
           <center><img width="133px" height="112px" src="img/sneakers/nike1.jpg" /></center>
           <div className="width">
           <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -40,6 +192,9 @@ function App() {
         </div>
 
         <div className="card">
+          <div className="favorite">
+            <a href="#" className="aLikes"></a>
+          </div>
           <center><img width="133px" height="112px" src="img/sneakers/2.jpg" /></center>
           <div className="width">
           <h5>Мужские Кроссовки Nike Air Max 270</h5>
@@ -54,6 +209,9 @@ function App() {
         </div>
 
         <div className="card">
+          <div className="favorite">
+            <a href="#" className="aLikes"></a>
+          </div>
           <center><img width="133px" height="112px" src="img/sneakers/3.jpg" /></center>
           <div className="width">
           <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -68,6 +226,9 @@ function App() {
         </div>
 
         <div className="card">
+          <div className="favorite">
+            <a href="#" className="aLikes"></a>
+          </div>
           <center><img width="133px" height="112px" src="img/sneakers/4.jpg" /></center>
           <div className="width">
           <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
@@ -76,7 +237,7 @@ function App() {
               <span>Цена:</span>
               <b>8999 руб.</b>
             </div>
-            <button><img width="11px" height="11px" src="img/plus.svg" /></button>
+            <button><img width="11px" height="11px" src="img/plus.svg" alt="img"/></button>
           </div>
           </div>
         </div>
