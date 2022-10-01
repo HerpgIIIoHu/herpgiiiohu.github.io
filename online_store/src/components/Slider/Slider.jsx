6c2554fc9336e1005b36ee0425480f1ad51d8568
Konstantin <kolupaevkonstantin18@gmail.com>
2022-10-01 14:55:16 +0700

    Немного адаптировал под телефон

--- online_store/src/components/Slider/Slider.jsx
+++ online_store/src/components/Slider/Slider.jsx
@@ -10,60 +10,36 @@ function Slider() {
         <div className={styles.divSlider}>
           <img className={styles.next} src="img/next.svg" alt="nextSlide" />
           <img className={styles.prev} src="img/prev.png" alt="prevSlide" />
-          <img className={styles.adidas} src="img/adidas.png" alt="logo" />
           
           <img
             className={styles.slider}
-            src="img/slider-frog.png"
+            src="img/sliders.png"
             alt="fontImg"
           />
-          <Carousel.Caption>
-            <div className={styles.hki}>
-              <h1 style={{ color: "#8BB43C" }}>Stan Smith,</h1>
-              <h1 style={{ color: "#000000" }}>Forever1!</h1>
-              <button className="green-btn">Купить</button>
-            </div>
-          </Carousel.Caption>
         </div>
       </Carousel.Item>
       <Carousel.Item>
         <div className={styles.divSlider}>
           <img className={styles.next} src="img/next.svg" alt="nextSlide" />
           <img className={styles.prev} src="img/prev.png" alt="prevSlide" />
-          <img className={styles.adidas} src="img/adidas.png" alt="logo" />
           
           <img
             className={styles.slider}
-            src="img/slider-frog.png"
+            src="img/sliders.png"
             alt="fontImg"
           />
-          <Carousel.Caption>
-            <div className={styles.hki}>
-              <h1 style={{ color: "#8BB43C" }}>Stan Smith,</h1>
-              <h1 style={{ color: "#000000" }}>Forever2!</h1>
-              <button className="green-btn">Купить</button>
-            </div>
-          </Carousel.Caption>
         </div>
       </Carousel.Item>
       <Carousel.Item>
         <div className={styles.divSlider}>
           <img className={styles.next} src="img/next.svg" alt="nextSlide" />
           <img className={styles.prev} src="img/prev.png" alt="prevSlide" />
-          <img className={styles.adidas} src="img/adidas.png" alt="logo" />
           
           <img
             className={styles.slider}
-            src="img/slider-frog.png"
+            src="img/sliders.png"
             alt="fontImg"
           />
-          <Carousel.Caption>
-            <div className={styles.hki}>
-              <h1 style={{ color: "#8BB43C" }}>Stan Smith,</h1>
-              <h1 style={{ color: "#000000" }}>Forever3!</h1>
-              <button className="green-btn">Купить</button>
-            </div>
-          </Carousel.Caption>
         </div>
       </Carousel.Item>
     </Carousel>
